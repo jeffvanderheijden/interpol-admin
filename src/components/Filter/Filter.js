30f0befcfad726807468070dc194562a02fa21d0
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 14:36:44 +0200

    Added logout functionality

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -22,7 +22,7 @@ const Filter = ({
 
     const logout = async () => {
         try {
-            const response = await fetch('https://api.interpol.sd-lab.nl/api/logout.php', {
+            const response = await fetch('https://api.interpol.sd-lab.nl/api/logout', {
                 method: 'POST',
                 credentials: 'include' // Include cookies in the request
             });

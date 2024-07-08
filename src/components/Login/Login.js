e601b461e9e1651f0ecbb240d344d54585886643
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 11:22:16 +0200

    Forgot to preventDefault on form submit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,10 @@ import "./Login.css";
 const Login = () => {
     // LDAP login form
     async function submitForm(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('username', e.target.elements.username.value);
+        formData.append('password', e.target.elements.password.value);
         try {
             const response = await fetch('https://api.interpol.sd-lab.nl/api/create-session', {
                 method: 'POST',

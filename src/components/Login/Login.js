c25277a1f17e71390943729ec251f9e63f63adb4
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 11:13:55 +0200

    Testing ldap login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+    // LDAP login form
+    async function submitForm(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('username', e.target.elements.username.value);
+        formData.append('password', e.target.elements.password.value);
+        
+        try {
+            const response = await fetch('https://api.interpol.sd-lab.nl/api/create-session', {
+                method: 'POST',
+                body: formData
+            });
+            const login = response.body;
+            console.log(login);
+        } catch (error) {
+            console.error('Error creating team:', error);
+        }
+    }
+
+    return (
+        <form id="loginForm" onSubmit={(e) => { submitForm(e) }}>
+            <input type="text" id="username" placeholder="Username" />
+            <input type="password" id="password" placeholder="Password" />
+            <button type="submit" className="btn">Inloggen</button>
+        </form>
+    );
+}
+
+export default Login;
---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../helpers/styles/reset.css";
+import Login from "../components/Login/Login";
+
+const LoginPage = () => {
+    return (
+        <Login />
+    )
+}
+
+export default LoginPage
+
+export const Head = () => <title>Login</title>

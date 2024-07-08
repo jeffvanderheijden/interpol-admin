1d451add5937f8f01ef4e15a3fd4132b887af21c
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-08 11:57:55 +0200

    Forgot build

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
             }
 
         } catch (error) {
-            console.error('Error creating session:', JSON.parse(error));
+            console.error('Error creating session:', error);
         }
     }
 

16a229441db129915ec7dd0494a676e5f5093b14
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-11-08 10:07:13 +0100

    Admin panel fixes

--- src/helpers/icons/Eye.js
+++ src/helpers/icons/Eye.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Eye = () => {
+const Eye = ({
+    onClick
+}) => {
     return (        
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 442.04 442.04">
+        <svg onClick={onClick} width="30" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 442.04 442.04">
             <g>
                 <g>
                     <path d="M221.02,341.304c-49.708,0-103.206-19.44-154.71-56.22C27.808,257.59,4.044,230.351,3.051,229.203

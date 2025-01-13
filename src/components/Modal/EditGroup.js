77ab6e48369b11eb86aafe38dd93db325ab1b28b
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-01-13 10:26:25 +0100

    Debugging update route

--- src/components/Modal/EditGroup.js
+++ src/components/Modal/EditGroup.js
@@ -117,7 +117,7 @@ const EditGroup = ({
 
     const saveGroupChanges = async (e) => {
         e.preventDefault();
-        console.log(e.target.elements.teamName)
+        console.log(e.target.elements.teamName.value);
         const formData = new FormData();
         formData.append('image', image);
         formData.append('name', e.target.elements.teamName.value);
